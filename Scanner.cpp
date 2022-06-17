b9189751aa0c413dd7438ebea11e28992aec5637
jack <84483508+hand-burger@users.noreply.github.com>
2022-06-17 11:41:28 -0400

    🥓 Scanner!

--- Scanner.cpp
+++ Scanner.cpp
@@ -4,28 +4,29 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include "solver.hpp"
 
 using namespace std;
 using namespace cv;
 
 Mat yellowMask, blueMask, orangeMask, redMask, greenMask, whiteMask;
 
-array<array<array<int, 3>, 3>, 6> cube{ {
-	{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Yellow side - Top side
-{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Orange side - Left side
-{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Blue side - Front side
-{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Red side - Right side
-{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Green side - Back side
-{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // White side - Bottom side
-	} };
-
 array<Mat, 6> faceScans;
 
 // Scan cube prototype
-void scanCube();
+void scanCube(array<array<array<int, 3>, 3>, 6> cube);
 
 int main()
 {
+	array<array<array<int, 3>, 3>, 6> cube = { {
+		{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Yellow side - Top side
+	{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Orange side - Left side
+	{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Blue side - Front side
+	{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Red side - Right side
+	{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // Green side - Back side
+	{ { { -1, -1, -1 },{ -1, -1, -1 },{ -1, -1, -1 } } }, // White side - Bottom side
+		} };
+
 	Mat img;
 	Mat yellow, blue, orange, red, green, white;
 
@@ -41,8 +42,14 @@ int main()
 
 		// User tutorial
 		if (!tutorialToggle) {
-			putText(img, "Rubik's Cube Solver!", Point(100, 100), FONT_HERSHEY_PLAIN, 3.0, Scalar(255, 128, 128), 4.0);
-			putText(img, "Press Space to scan your cube", Point(100, 170), FONT_HERSHEY_PLAIN, 2.0, Scalar(255, 128, 128), 2.5);
+			putText(img, "Rubik's Cube Solver!", Point(85, 100), FONT_HERSHEY_PLAIN, 3.0, Scalar(0, 0, 0), 5.0);
+			putText(img, "Rubik's Cube Solver!", Point(85, 100), FONT_HERSHEY_PLAIN, 3.0, Scalar(255, 255, 255), 4.5);
+			putText(img, "Press Space to scan your cube", Point(85, 170), FONT_HERSHEY_PLAIN, 2.0, Scalar(0, 0, 0), 5.5);
+			putText(img, "Press Space to scan your cube", Point(85, 170), FONT_HERSHEY_PLAIN, 2.0, Scalar(255, 128, 128), 3.0);
+			putText(img, "Hold the Rubik's cube with the white face ", Point(30, 215), FONT_HERSHEY_PLAIN, 1.5, Scalar(0, 0, 0), 3.0);
+			putText(img, "Hold the Rubik's cube with the white face ", Point(30, 215), FONT_HERSHEY_PLAIN, 1.5, Scalar(255, 128, 128), 2.5);
+			putText(img, "towards yourself and orange on top.", Point(30, 235), FONT_HERSHEY_PLAIN, 1.5, Scalar(0, 0, 0), 3.0);
+			putText(img, "towards yourself and orange on top.", Point(30, 235), FONT_HERSHEY_PLAIN, 1.5, Scalar(255, 128, 128), 2.5);
 
 			if (waitKey(5) == 32) {
 				tutorialToggle = !tutorialToggle;
@@ -51,7 +58,7 @@ int main()
 
 		// Scan yellow side
 		if (tutorialToggle && !yellowToggle) {
-			putText(img, "Scan Yellow Side", Point(100, 100), FONT_HERSHEY_PLAIN, 5.0, Scalar(0, 215, 255), 5);
+			putText(img, "Scan Yellow Side", Point(100, 100), FONT_HERSHEY_PLAIN, 3.0, Scalar(0, 215, 255), 3);
 			rectangle(img, Point(img.cols / 2 - 100, img.rows / 2 - 100), Point(img.cols / 2 + 100, img.rows / 2 + 100), Scalar(0, 215, 255), 5);
 
 			if (waitKey(5) == 32) {
@@ -67,8 +74,9 @@ int main()
 
 		// Scan orange side
 		if (yellowToggle && !orangeToggle) {
-			putText(img, "Scan Orange Side", Point(100, 100), FONT_HERSHEY_PLAIN, 5.0, Scalar(0, 165, 255), 5);
+			putText(img, "Scan Orange Side", Point(100, 100), FONT_HERSHEY_PLAIN, 3.0, Scalar(0, 165, 255), 3);
 			rectangle(img, Point(img.cols / 2 - 100, img.rows / 2 - 100), Point(img.cols / 2 + 100, img.rows / 2 + 100), Scalar(0, 165, 255), 5);
+			putText(img, "Move the entire cube forwards", Point(10, 400), FONT_HERSHEY_PLAIN, 2.25, Scalar(0, 165, 255), 2.5);
 
 			if (waitKey(5) == 32) {
 				orangeToggle = !orangeToggle;
@@ -83,8 +91,9 @@ int main()
 
 		// Scan blue side
 		if (orangeToggle && !blueToggle) {
-			putText(img, "Scan Blue Side", Point(100, 100), FONT_HERSHEY_PLAIN, 5.0, Scalar(255, 10, 10), 5);
+			putText(img, "Scan Blue Side", Point(100, 100), FONT_HERSHEY_PLAIN, 3.0, Scalar(255, 10, 10), 3);
 			rectangle(img, Point(img.cols / 2 - 100, img.rows / 2 - 100), Point(img.cols / 2 + 100, img.rows / 2 + 100), Scalar(255, 10, 10), 5);
+			putText(img, "Turn the cube to the left", Point(10, 400), FONT_HERSHEY_PLAIN, 2.25, Scalar(255, 10, 10), 2.5);
 
 			if (waitKey(5) == 32) {
 				blueToggle = !blueToggle;
@@ -99,8 +108,9 @@ int main()
 
 		// Scan red side
 		if (blueToggle && !redToggle) {
-			putText(img, "Scan Red Side", Point(100, 100), FONT_HERSHEY_PLAIN, 5.0, Scalar(0, 5, 255), 5);
+			putText(img, "Scan Red Side", Point(100, 100), FONT_HERSHEY_PLAIN, 3.0, Scalar(0, 5, 255), 3);
 			rectangle(img, Point(img.cols / 2 - 100, img.rows / 2 - 100), Point(img.cols / 2 + 100, img.rows / 2 + 100), Scalar(0, 5, 255), 5);
+			putText(img, "Turn the cube to the left", Point(10, 400), FONT_HERSHEY_PLAIN, 2.25, Scalar(0, 5, 255), 2.5);
 
 			if (waitKey(5) == 32) {
 				redToggle = !redToggle;
@@ -115,8 +125,9 @@ int main()
 
 		// Scan green side
 		if (redToggle && !greenToggle) {
-			putText(img, "Scan Green Side", Point(100, 100), FONT_HERSHEY_PLAIN, 5.0, Scalar(0, 255, 5), 5);
+			putText(img, "Scan Green Side", Point(100, 100), FONT_HERSHEY_PLAIN, 3.0, Scalar(0, 255, 5), 3);
 			rectangle(img, Point(img.cols / 2 - 100, img.rows / 2 - 100), Point(img.cols / 2 + 100, img.rows / 2 + 100), Scalar(0, 255, 5), 5);
+			putText(img, "Turn the cube to the left", Point(10, 400), FONT_HERSHEY_PLAIN, 2.25, Scalar(0, 255, 5), 2.5);
 
 			if (waitKey(5) == 32) {
 				greenToggle = !greenToggle;
@@ -131,8 +142,9 @@ int main()
 
 		// Scan white side
 		if (greenToggle && !whiteToggle) {
-			putText(img, "Scan White Side", Point(100, 100), FONT_HERSHEY_PLAIN, 5.0, Scalar(255, 255, 255), 5);
+			putText(img, "Scan White Side", Point(100, 100), FONT_HERSHEY_PLAIN, 3.0, Scalar(255, 255, 255), 3);
 			rectangle(img, Point(img.cols / 2 - 100, img.rows / 2 - 100), Point(img.cols / 2 + 100, img.rows / 2 + 100), Scalar(255, 255, 255), 5);
+			putText(img, "Turn the entire cube forwards", Point(10, 400), FONT_HERSHEY_PLAIN, 2.25, Scalar(255, 255, 255), 2.5);
 
 			if (waitKey(5) == 32) {
 				whiteToggle = !whiteToggle;
@@ -144,23 +156,9 @@ int main()
 				faceScans[5] = white(ROI);
 
 				// Now all the sides are scanned so now do some logic
-				scanCube();
+				scanCube(cube);
 			}
 		}
-
-		// Print the cube array
-		for (size_t i = 0; i < cube.size(); ++i)
-		{
-			for (size_t j = 0; j < cube[0].size(); ++j)
-			{
-				for (size_t k = 0; k < cube[0][0].size(); ++k)
-				{
-					cout << cube[i][j][k] << ", ";
-				}
-			}
-			cout << endl;
-		}
-		cout << endl;
 
 		imshow("Frame", img);
 
@@ -169,7 +167,7 @@ int main()
 	return 0;
 }
 
-void scanCube()
+void scanCube(array<array<array<int, 3>, 3>, 6> cube)
 {
 	// Now loop over all nine points for each color
 
@@ -201,7 +199,7 @@ void scanCube()
 		inRange(faceScans[i], Scalar(18, 134, 45), Scalar(40, 238, 237), faceMasks[0]);
 		dilate(faceMasks[0], faceMasks[0], kernel);
 		// Orange mask
-		inRange(faceScans[i], Scalar(0, 59, 83), Scalar(19, 255, 255), faceMasks[1]);
+		inRange(faceScans[i], Scalar(0, 50, 75), Scalar(19, 255, 255), faceMasks[1]);
 		dilate(faceMasks[1], faceMasks[1], kernel);
 		// Blue mask
 		inRange(faceScans[i], Scalar(104, 135, 43), Scalar(122, 255, 210), faceMasks[2]);
@@ -210,13 +208,11 @@ void scanCube()
 		inRange(faceScans[i], Scalar(0, 132, 0), Scalar(9, 255, 178), faceMasks[3]);
 		dilate(faceMasks[3], faceMasks[3], kernel);
 		// Green mask
-		inRange(faceScans[i], Scalar(60, 45, 50), Scalar(76, 255, 158), faceMasks[4]);
+		inRange(faceScans[i], Scalar(60, 45, 50), Scalar(76, 255, 170), faceMasks[4]);
 		dilate(faceMasks[4], faceMasks[4], kernel);
 		// White mask
 		inRange(faceScans[i], Scalar(101, 13, 76), Scalar(120, 160, 255), faceMasks[5]);
 		dilate(faceMasks[5], faceMasks[5], kernel);
-
-		cout << i << endl;
 
 		// Now that all six masks are made, each color of the nine colors need to be checked on each of the six masks
 
@@ -241,6 +237,20 @@ void scanCube()
 		}
 	}
 
-	// Now the cube is scanned and the cube array is filled (Hopefully)
-	
+	// Now change the positions of the cubies in the array so that it matches the format of the solver
+	array<array<array<int, 3>, 3>, 6> copyOfCube;
+	copyOfCube = cube;
+
+
+
+	// Do a little bit of error correction
+
+	// Change the center pieces to their respective color because the scan is based of the center
+	for (int i = 0; i < 6; i++)
+	{
+		cube[i][1][1] = i;
+	}
+
+	// Now the cube is scanned and the cube array is filled with numbers that are not -1 (Hopefully)
+	solveCube(cube);
 }
